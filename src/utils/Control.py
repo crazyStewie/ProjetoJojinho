66d5282a68918c8bbc212b991778096f890f32f1
Rodrigo Tanaka <krodrigo71008@gmail.com>
2019-03-08 15:08:58 -0300

    More folder structure improvements

---
+++ src/utils/Control.py
@@ -0,0 +1,12 @@
+class Control:
+    def __init__(self):
+        pass
+
+    def is_pressed(self, action):
+        pass
+
+    def just_pressed(self, action):
+        pass
+
+    def just_released(self, action):
+        pass
