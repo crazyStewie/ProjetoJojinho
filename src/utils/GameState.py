0eef1b744802fb347905461c258988d294a60b81
JoelNS <joelgbsilva@gmail.com>
2019-03-16 17:36:00 -0300

    Initial GameState class mockup

---
+++ src/utils/GameState.py
@@ -0,0 +1,18 @@
+class GameState:
+    def __init__(self):
+        pass
+
+    def get_player_count(self):
+        pass
+
+    def get_player_fuel(self, player_id):
+        pass
+
+    def get_player_score(self, player_id):
+        pass
+
+    def get_game_state(self):
+        pass
+
+
+game_state = GameState()
