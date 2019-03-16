38e4ddd96310d77d6d435a6d7860d746b0c08517
Rodrigo Tanaka <krodrigo71008@gmail.com>
2019-03-16 17:42:27 -0300

    Improving box drawing

--- src/gui/GUI.py
+++ src/gui/GUI.py
@@ -11,14 +11,6 @@ class GUI:
         pyglet.resource.reindex()
         self.active_elements = []
         self.passive_elements = []
-        button_box_image = pyglet.resource.image("button_box.png")
-        button_box_image.anchor_x = button_box_image.width // 2
-        button_box_image.anchor_y = button_box_image.height // 2
-        selector_box_image = pyglet.resource.image("selector_box.png")
-        selector_box_image.anchor_x = selector_box_image.width // 2
-        selector_box_image.anchor_y = selector_box_image.height // 2
-        self.button_box = pyglet.sprite.Sprite(button_box_image)
-        self.selector_box = pyglet.sprite.Sprite(selector_box_image)
         self.focus = -1
 
     def setup_initial_menu(self):
@@ -42,18 +34,19 @@ class GUI:
         self.active_elements = []
         self.passive_elements = []
 
+    @staticmethod
+    def draw_box(corner1, corner2):
+        pyglet.graphics.draw(5, pyglet.gl.GL_LINE_STRIP, ("v2f", (corner1[0], corner1[1],
+                                                                  corner2[0], corner1[1],
+                                                                  corner2[0], corner2[1],
+                                                                  corner1[0], corner2[1],
+                                                                  corner1[0], corner1[1])))
+
     def update(self):
         if Control.control.just_released("ui_up"):
             self.focus = max(0, self.focus - 1)
         elif Control.control.just_released("ui_down"):
             self.focus = min(4, self.focus + 1)
-        if type(self.active_elements[self.focus]) is Button:
-            self.button_box.x = self.active_elements[self.focus].x
-            self.button_box.y = self.active_elements[self.focus].y
-        elif type(self.active_elements[self.focus]) is Selector:
-            self.selector_box.x = self.active_elements[self.focus].x
-            self.selector_box.y = self.active_elements[self.focus].y
-
         for index, elem in enumerate(self.active_elements):
             if index == self.focus:
                 elem.is_focused = True
@@ -66,7 +59,13 @@ class GUI:
             elem.draw()
         for elem in self.active_elements:
             elem.draw()
-        if type(self.active_elements[self.focus]) is Button:
-            self.button_box.draw()
-        elif type(self.active_elements[self.focus]) is Selector:
-            self.selector_box.draw()
+
+        corner1 = (self.active_elements[self.focus].x -
+                   self.active_elements[self.focus].default_sprite.width//2 - consts.BOX_MARGIN,
+                        self.active_elements[self.focus].y -
+                    self.active_elements[self.focus].default_sprite.height // 2 - consts.BOX_MARGIN)
+        corner2 = (self.active_elements[self.focus].x +
+                   self.active_elements[self.focus].default_sprite.width // 2 + consts.BOX_MARGIN,
+                   self.active_elements[self.focus].y +
+                   self.active_elements[self.focus].default_sprite.height // 2 + consts.BOX_MARGIN)
+        self.draw_box(corner1, corner2)
