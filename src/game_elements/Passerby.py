3c500523d66d8412b73bbd0ccce059875276c5da
Rodrigo Tanaka <krodrigo71008@gmail.com>
2019-03-28 13:52:31 -0300

    Adding Passerby class

---
+++ src/game_elements/Passerby.py
@@ -0,0 +1,5 @@
+class Passerby:
+    def __init__(self, street, relative_position, direction):
+        self.street = street
+        self.relative_position = relative_position
+        self.direction = direction
